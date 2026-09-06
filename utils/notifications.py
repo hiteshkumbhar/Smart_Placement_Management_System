9bdbd4b8aef6164ed14962491996495bab540e88
Hitesh Kumbhar <hiteshkumbhar29@gmail.com>
2026-09-06 19:09:53 +0530

    Updated

--- utils/notifications.py
+++ utils/notifications.py
@@ -74,9 +74,9 @@ def notifications(mode):
         email = mode[1]
         html_message = f"""
         Congratulations!
-        \n\n
+        <br><br>
         Your resume has been verified.
-\n\n
+        <br><br>
         You are now eligible to apply for placement drives."""
         return send_email(
             email,
